cca1f3943cc907bfdc2060732e308e317d28bd7e
Hasin Ishraq <hasinishraq26@gmail.com>
2024-03-05 20:58:47 +0600

    Initial commit

---
+++ src/main/java/org/example/demo/HelloController.java
@@ -0,0 +1,50 @@
+package org.example.demo;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+
+public class HelloController {
+
+    @FXML
+    private Button clickme;
+
+    @FXML
+    private Label showtext;
+
+    @FXML
+    private TextField textinput;
+
+    @FXML
+    private ImageView imageid;
+
+    @FXML
+    public void clickButton(){
+        String alisha = textinput.getText();
+
+        //getting text value from scenebuilder textfield  and store in javafx alisha variable
+        //getText() built in function
+
+        showtext.setText(alisha);
+        //show the alisha text in label showtext
+
+        try {
+            Image image = new Image(new FileInputStream("src/main/resources/cat.jpg"));
+            imageid.setImage(image);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
